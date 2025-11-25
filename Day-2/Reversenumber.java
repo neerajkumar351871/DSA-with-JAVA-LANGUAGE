06300631282b4cb860b54d8f64207b9bcf29c7c5
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-11-25 22:43:52 +0530

    Reverse number

---
+++ Day-2/Reversenumber.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Reversenumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int reversenumber = 0;
+        while (number > 0) {
+            int lastdigit = number % 10;
+            reversenumber = reversenumber * 10 + lastdigit;
+            number /= 10;
+        }
+
+        System.out.println("Reverse number is : " + reversenumber);
+    }
+}
