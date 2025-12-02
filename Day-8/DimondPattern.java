ec7d92d516a0e6b9c1481b863aad49647f6941ab
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-12-02 22:30:21 +0530

    Dimond pattern

---
+++ Day-8/DimondPattern.java
@@ -0,0 +1,36 @@
+
+public class DimondPattern {
+
+    public static void DimondPattern(int n) {
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop  / Space
+            for (int spc = 0; spc < (n - i); spc++) {
+                System.out.print(" ");
+            }
+            // Str
+            for (int str = 1; str <= (i * 2) - 1; str++) {
+                System.out.print("*");
+            }
+            // Reverse
+            System.out.println("");
+        }
+          for (int i = n; i >= 1; i--) {
+            // inner loop  / Space
+            for (int spc = 0; spc < (n - i); spc++) {
+                System.out.print(" ");
+            }
+            // Str
+            for (int str = 1; str <= (i * 2) - 1; str++) {
+                System.out.print("*");
+            }
+            // Reverse
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DimondPattern(4);
+    }
+}
