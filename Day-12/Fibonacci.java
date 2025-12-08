86916f05bf92ab6e805879f67c0d24d0687a4c74
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-12-08 22:54:52 +0530

    Fibnacci

---
+++ Day-12/Fibonacci.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class Fibonacci {
+
+    public static void Fibonacciseries(int n) {
+
+        int a = 0, b = 1;
+
+       System.out.print("Fibonacci Series: " + a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+        Fibonacciseries(n);
+
+    }
+}
