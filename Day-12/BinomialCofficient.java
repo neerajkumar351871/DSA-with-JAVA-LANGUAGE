7aa29c116bb16134bbb047c3ebb794fc86e2938b
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-12-08 22:13:22 +0530

    Binomial Cofficient

---
+++ Day-12/BinomialCofficient.java
@@ -0,0 +1,34 @@
+public class  BinomialCofficient
+{   
+    public static int factorail(int n)
+{
+    int fact=1;
+    for(int i=1;i<=n;i++)
+    {
+        fact*=i;
+    }
+  return  fact;
+} 
+ public static  int BinomialCofficient(int n, int r)
+{
+    // Calculate n!
+    int numFact=factorail(n);
+    // Calculate r!
+    int rfact=factorail(r);
+    // Calculate (n-r)!
+    int n_rfact=factorail(n-r);
+    // calculate Binomial Cofficient
+    int binomialcofficient=numFact/(rfact*n_rfact);
+
+
+return binomialcofficient;
+}
+
+    public static void main(String[] args) {
+        int n=7;
+        int r=3;
+        int result=BinomialCofficient(n, r);
+        System.out.println("Factorail of this number:"+result);
+        
+    }
+}
