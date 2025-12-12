3563002680130ba95da8b6b301d4fe237a763e1c
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-12-12 22:03:40 +0530

    Pairs in array

---
+++ Day-16/Pairsinarray.java
@@ -0,0 +1,17 @@
+public class Pairsinarray
+{  public  static void Pairsinarray(int  array[])
+{
+   for(int i=0;i<array.length;i++)
+   {
+     for(int j=i+1;j<array.length;j++)
+     {
+        System.out.print("("+array[i]+","+array[j]+")");
+     }
+     System.out.println("");
+   }
+}
+    public static void main(String[] args) {
+        int array[]={2,4,6,8,10};
+        Pairsinarray(array);
+    }
+}
