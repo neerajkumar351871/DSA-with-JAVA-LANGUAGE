2336c45bdfb58174e62e3f274055ceeb24d314d0
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-09 20:03:18 +0530

    Revesion

---
+++ Revision/Bitwiseoperators.java
@@ -0,0 +1,24 @@
+public class Bitwiseoperators {
+
+    public static void ANDOperator() {
+        System.out.println("Bitwise  AND operator :"+(5&4));
+       
+    }
+    public static void ORoperator()
+    {
+        System.out.println("Bitwise OR Operator :"+(5|6));
+    }
+     public static void XORoperator()
+    {
+        System.out.println("Bitwise XOR Operator :"+(5^6));
+    }
+
+
+    public static void main(String[] args) {
+        ANDOperator();
+        ORoperator();
+        XORoperator();
+    
+       
+    }
+}
