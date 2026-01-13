5ae21aa93d31f7715a6cda6c16e28b550760c0e3
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-13 21:16:36 +0530

    Set ith  bit

--- Day-23/get_ith_bit.java
+++ Day-23/get_ith_bit.java
@@ -1,20 +1,18 @@
-public class get_ith_bit
-{  
-    public static int get_ith_bit(int n , int i)
-    {
-        int bitmask=1<<i;
-        if((n & bitmask)==0)
-        {
-            return  0;
+
+public class get_ith_bit {
+
+    public static int get_ith_bit(int n, int i) {
+        int bitmask = 1 << i;
+        if ((n & bitmask) == 0) {
+            return 0;
+        } else {
+            return 1;
         }
-        else{
-    return  1;
-        }
-    
+
     }
 
     public static void main(String[] args) {
-    System.out.println(get_ith_bit(10, 2));
-        
+        System.out.println(get_ith_bit(17, 3));
+
     }
 }
