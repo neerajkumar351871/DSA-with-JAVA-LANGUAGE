64afc6c575428cc7aca0f8bae8b9f0892dd53e82
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-15 10:43:55 +0530

    Practice question

---
+++ Revision/Practice.java
@@ -0,0 +1,16 @@
+public  class Practice
+{
+    public static  void main(String args[])
+    {
+        int a=5;
+         System.out.println("a++ : "+(a++));// print a=5 value 6;
+          System.out.println("a : "+(a)); //value 6;
+         System.out.println("++a : "+(++a));// print 7 value 7,
+         System.out.println("a : "+(a)); //value 7
+         System.out.println("a-- : "+(a--));// Print 7 value 6
+         System.out.println("a : "+(a)); // value 6
+         System.out.println("--a : "+(a--));// 6 value 5
+          System.out.println("a : "+(a)); // value 6
+
+    }
+}
