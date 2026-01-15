0b760bc25c306d2761cd72826f1b8bab25735a52
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-15 22:56:39 +0530

    Count set bit

---
+++ Day-26/Countsetbit.java
@@ -0,0 +1,18 @@
+public class Countsetbit
+{  public  static int Countsetbit(int n)
+{
+    int count =0;
+     while(n>0)
+     {
+        if((n& 1)!=0)
+        {
+           count++;
+        }
+        n=n>>1;
+     }
+     return  count;
+}
+    public static void main(String[] args) {
+         System.out.println(Countsetbit(15));
+    }
+}
