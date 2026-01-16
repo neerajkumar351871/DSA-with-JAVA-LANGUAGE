a9035d9c787b49fa8751c5d435f8cfd82f84b38f
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-16 10:55:33 +0530

    class and obeject

---
+++ Day-27/Class_Object.java
@@ -0,0 +1,33 @@
+
+
+
+public class Class_Object {
+
+    public static void main(String[] args) {
+
+        // classname  objectname  =  new keyword   classname with parenthesis()
+        Pen data = new Pen();
+        data.setcolor("Red");
+        System.out.println(data.color);
+         data.settips(5);
+         System.out.println(data.tip);
+
+    }
+}
+// Here class Declare
+
+class Pen {
+
+    String color;
+    int tip;
+    // Function set color;
+
+    void setcolor(String newcolor) {
+        color = newcolor;
+    }
+
+    // Function settips.
+    void settips(int newtipes) {
+        tip = newtipes;
+    }
+}
