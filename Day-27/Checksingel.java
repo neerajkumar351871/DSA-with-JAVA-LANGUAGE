916dc0c08005755b380eb0479e6bd334aa7e9d86
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-16 22:24:02 +0530

    check singel value i  array

---
+++ Day-27/Checksingel.java
@@ -0,0 +1,15 @@
+public class Checksingel {
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println("Single Number: " + singleNumber(nums));
+    }
+}
