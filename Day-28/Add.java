61d4a2663266821078c3ce5e78c6ac3cc51348e6
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-18 23:25:47 +0530

    Day -28 complete

---
+++ Day-28/Add.java
@@ -0,0 +1,13 @@
+class Add {
+    public static void main(String[] args) {
+        int a = 5, b = 7;
+
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        System.out.println("Sum = " + a);
+    }
+}
