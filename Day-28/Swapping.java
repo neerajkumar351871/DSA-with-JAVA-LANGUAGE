20d891ce31c707fb24482cd07156357be4fc501d
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-18 23:16:08 +0530

    Using bitwise operator

---
+++ Day-28/Swapping.java
@@ -0,0 +1,17 @@
+class  Swapping {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swap:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        // Swapping using XOR
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swap:");
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
