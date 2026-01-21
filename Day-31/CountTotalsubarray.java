76b740aab3ca60a540950a5b70aa88d3c39dee61
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-21 13:23:20 +0530

    Some  change

--- Day-31/CountTotalsubarray.java
+++ Day-31/CountTotalsubarray.java
@@ -14,10 +14,23 @@ public class CountTotalsubarray {
             }
             System.out.println("");
             }
-            System.out.println("Total sub array : "+subCount);
-            System.out.println("Total sub array Iteration : "+subarray);
+           // System.out.println("Total sub array : "+subCount);
+           // System.out.println("Total sub array Iteration : "+subarray);
 
+       // Function  call
+           subCounts(array.length);
+           CountIteration(array.length);
+    }
+    public static void subCounts(int n)
+    {
+        int subarrays=n*(n+1)/2;
+        System.out.println("Total sub array: "+subarrays);
 
+    }
+    public static void CountIteration(int n)
+    {
+        int Iteration = n*(n+1)*(n+2)/6;
+        System.out.println("Total iteration is:"+Iteration);
     }
 
     public static void main(String[] args) {
