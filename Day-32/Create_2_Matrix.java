ca671b78d8a136a61d1787ff1eef83054599aa4e
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-22 23:36:49 +0530

    Search value in 2d array

--- Day-32/Create_2_Matrix.java
+++ Day-32/Create_2_Matrix.java
@@ -1,28 +1,41 @@
 
 import java.util.Scanner;
 
-public class Create_2_Matrix
-{
-    public static void main(String[] args) {
-        int matrix[][]= new int [3][3];
-         int  n=matrix.length, m=matrix[0].length;
-         Scanner sc= new Scanner(System.in);
-          for(int i=0;i<n;i++)
-          {
-            for(int j=0;j<m;j++)
-            {
-                matrix[i][j]= sc.nextInt();
+public class Create_2_Matrix {
+
+    public static boolean Searchvalue(int matrix[][], int search) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == search) {
+                    return true;
+                }
             }
-          }
-          // Print 2d array
-          for(int i=0;i<n;i++)
-          {
-            for(int j=0;j<m;j++)
-            {
-                System.err.print(matrix[i][j]+" ");
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        // Print 2d array
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.err.print(matrix[i][j] + " ");
             }
             System.out.println("");
-          }
+        }
+        int ser = 4;
+        if (Searchvalue(matrix, ser)) {
+            System.out.println("Valu is founf:" + ser);
+        } else {
+            System.out.println("Value is not found");
+        }
 
     }
 }
