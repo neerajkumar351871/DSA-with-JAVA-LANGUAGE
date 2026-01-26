12f5751de824cf797e7c05641507131ed0ac4ab1
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-26 22:37:49 +0530

    Insertion sort

---
+++ Day35/InsertionSort.java
@@ -0,0 +1,30 @@
+public class InsertionSort
+{ 
+    public static void InsertionSort(int array[])
+    {
+        for(int i=1;i<array.length;i++)
+    {
+        int current= array[i];
+        int previous=i-1;
+        while(previous>=0 && array[previous]> current)
+        {
+            array[previous+1]= array[previous];
+            previous--;
+        }
+      // Inserion;
+      array[previous+1]= current;
+       } 
+    }
+    public static void printarry(int array[])
+    {
+        for(int i=0;i<array.length;i++)
+    {
+        System.out.print(array[i]+" ");
+    }
+    }
+  public static void main(String[] args) {
+      int array[]={5,4,1,3,2};
+      InsertionSort(array);
+      printarry(array);
+  }  
+}
