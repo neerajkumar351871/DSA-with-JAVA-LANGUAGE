c6ebcc37d79b5173478832b44f695046b4e5614a
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-27 22:41:32 +0530

    Count sort

---
+++ Day-36/CountingSort.java
@@ -0,0 +1,35 @@
+
+public class CountingSort {
+
+    public static void CountingSort(int array[]) {  // Find  largest value.
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            largest = Math.max(largest, array[i]);
+        }
+        int count[] = new int[largest + 1]; // include the value is Zero.
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]]++;
+        }
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                array[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int array[] = {1, 4, 1, 3, 2, 4, 3, 7};
+        CountingSort(array);
+        printarr(array);
+    }
+}
