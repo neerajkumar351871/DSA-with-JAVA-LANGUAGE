26d1d6f7650b89611a642876314a33b08e85a5cd
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-01-27 23:33:17 +0530

    Insertion sort for descending order

---
+++ Day-36/InsertionSortDescendinfOrder.java
@@ -0,0 +1,31 @@
+
+public class InsertionSortDescendinfOrder
+{ 
+    public static void InsertionSort(int array[])
+    {
+        for(int i=1;i<array.length;i++)
+    {
+        int current= array[i];
+        int previous=i-1;
+        while(previous>=0 && array[previous]< current)
+        {
+            array[previous+1]= array[previous];
+            previous--;
+        }
+      // Inserion;
+      array[previous+1]= current;
+       } 
+    }
+    public static void printarry(int array[])
+    {
+        for(int i=0;i<array.length;i++)
+    {
+        System.out.print(array[i]+" ");
+    }
+    }
+  public static void main(String[] args) {
+      int array[]={5,4,1,3,2};
+      InsertionSort(array);
+      printarry(array);
+  }  
+}
