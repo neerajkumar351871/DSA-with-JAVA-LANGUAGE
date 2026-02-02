0df1d282bf96edd27fe068c0e30e60e537728866
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-02-02 22:58:25 +0530

    palindrome String

---
+++ Day-41/palindrome.java
@@ -0,0 +1,20 @@
+    public class palindrome
+{ 
+    public static boolean paldromeString(String str)
+    {
+        int end= str.length()-1;
+        for(int i=0;i<str.length()/2;i++)
+        {
+           if(str.charAt(i)== str.charAt(end))
+           { 
+             end --;
+            return  true ;
+           }
+        }
+        return false;
+    }
+    public static void main(String args[])
+    {
+        System.out.print(paldromeString("noon"));
+    }
+}
