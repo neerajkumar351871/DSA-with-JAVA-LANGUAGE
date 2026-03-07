dcb9ecce460f70ffd51b371d2fcb518f5a4497ae
neerajkumar351871 <neerajkumar351871@gmail.com>
2026-03-07 11:13:59 +0530

    Package creat in java

---
+++ OOPS/Package/Test.java
@@ -0,0 +1,9 @@
+import mypackage.demo;
+public class Test
+{
+    public static void main(String args[])
+    {
+    demo s1 = new demo();
+     System.out.print(s1.add(2,3));
+    }
+}
---
+++ OOPS/Package/mypackage/demo.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class demo
+{
+   public int add(int a ,int b)
+   {
+    return a+b;
+   }
+}
